3a287bc54823a2e5594af612b400e0f47295ebe0
Hramcov Semen <gerasm666999@gmail.com>
2021-10-27 21:06:52 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2000000000;
+        int trans = 500000000;
+        int totalBalance = balance + trans;
+        System.out.println(totalBalance);
+    }
+}
