3359fef20de8731b12fb066ff6c84e553f1f2c0b
Hramcov Semen <gerasm666999@gmail.com>
2021-10-28 13:44:38 +0500

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int balance = 2000000000;
-        int trans = 500000000;
+        int balance = 2_000_000_000;
+        int trans = 500_000_000;
         int totalBalance = balance + trans;
         System.out.println(totalBalance);
     }
